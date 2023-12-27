e29dc8298523962806be3c9510eb9496b52cb7e8
SangHyun Cho <codynice9@gmail.com>
2023-12-27 16:35:11 +0900

    코멘트 추가

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -29,7 +29,9 @@ public class BoardController {
  * @ResponseBody, @RequestBody
  * Board example
 */
-
+	//해당 컨트롤러는 일반적인 형식으로 우선 처리한뒤 Rest API방식(BoardRestController.java)사용을위해 main()메소드를 제외한 모든메소드 주석처리
+	//jsp의 ajax메소드도 대거 수정 함
+	
 	//REST방식 적용을위해 주석처리. BoardRestController에 REST방식 적용한코드 기재
 //	@Autowired
 //	BoardMapper boardMapper;
